1ff3ea4b979651a84960a5c695c304fb9aba3f4d
mdmasharafilhossain <mashrafilmahi007@gmail.com>
2025-12-04 00:38:58 +0600

    Bug Fixed

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -1,5 +1,6 @@
-// import { AuthProvider } from "@/components/modules/auth/AuthContext";
 
+
+import { AuthProvider } from "@/components/modules/auth/AuthProvider/AuthProvider";
 import Footer from "@/components/shared/Footer";
 import Navbar from "@/components/shared/Navbar/Navbar";
 
@@ -15,13 +16,13 @@ export default function PublicLayout({
   return (
     <> 
     
-    {/* <AuthProvider> */}
+    <AuthProvider>
       <Navbar />
      
       <main className="min-h-dvh">{children}</main>
       
       <Footer />
-      {/* </AuthProvider> */}
+      </AuthProvider>
     </>
   );
 }
