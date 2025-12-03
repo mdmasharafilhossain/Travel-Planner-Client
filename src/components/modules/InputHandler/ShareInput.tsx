78257c431d0991f964907e68cd0ce5d2a4ffb134
mdmasharafilhossain <mashrafilmahi007@gmail.com>
2025-12-03 22:59:34 +0600

    React Hook Form Added

---
+++ src/components/modules/InputHandler/ShareInput.tsx
@@ -0,0 +1,25 @@
+"use client";
+import { UseFormRegisterReturn } from "react-hook-form";
+
+
+export default function RHFInput({
+  label,
+  register,
+  error,
+  type = "text",
+  placeholder
+}: {
+  label: string;
+  register: UseFormRegisterReturn;
+  error?: string | null;
+  type?: string;
+  placeholder?: string;
+}) {
+  return (
+    <div className="mb-3">
+      <label className="block text-sm font-medium mb-1">{label}</label>
+      <input {...register} type={type} placeholder={placeholder} className="w-full border p-2 rounded" />
+      <FormError message={error} />
+    </div>
+  );
+}
