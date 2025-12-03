d73c5c2a0d2cf486bc5ad21e52ec1121aa3912ed
mdmasharafilhossain <mashrafilmahi007@gmail.com>
2025-12-03 22:28:16 +0600

    Base Api Added

---
+++ src/lib/baseApi.ts
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+const API_BASE = process.env.NEXT_PUBLIC_API_BASE || "http://localhost:5000";
+
+export const axiosClient = axios.create({
+  baseURL: API_BASE,
+  withCredentials: true,
+  headers: { "Content-Type": "application/json" }
+});
