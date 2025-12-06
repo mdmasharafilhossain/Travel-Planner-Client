fb29fcb118ce358ef51a78e0b849110b00612da1
mdmasharafilhossain <mashrafilmahi007@gmail.com>
2025-12-06 10:08:06 +0600

    Admin Dashboard Added

---
+++ src/app/(adminLayout)/Admin/layout.tsx
@@ -0,0 +1,34 @@
+
+
+import { AuthProvider } from "@/components/modules/auth/AuthProvider/AuthProvider";
+import Sidebar from "@/components/shared/Navbar/Sidebar";
+
+import AuthWrapper from "@/lib/AuthWrapper";
+
+
+
+export default function DashboardLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  
+  return (
+    <AuthProvider>
+     <AuthWrapper>
+      <div className="min-h-screen bg-linear-to-br from-gray-50 via-[#F8F5FF] to-gray-100 dark:from-gray-950 dark:via-[#1a1033] dark:to-gray-900">
+        <div className="flex">
+          <Sidebar />
+          
+          <main className="flex-1 min-h-screen md:ml-0 p-4 sm:p-6 lg:p-8 overflow-x-hidden">
+           
+            <div className="md:mt-0 mt-16">
+              {children}
+            </div>
+          </main>
+        </div>
+      </div>
+   </AuthWrapper>
+    </AuthProvider>
+  );
+}
---
+++ src/app/(adminLayout)/Admin/user-management/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const UserManagementPage = () => {
+    return (
+        <div>
+            <h1>uoiieu</h1>
+        </div>
+    );
+};
+
+export default UserManagementPage;
