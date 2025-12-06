69fee256205ac31540e5d315dc38c9e934162ea1
mdmasharafilhossain <mashrafilmahi007@gmail.com>
2025-12-06 13:32:33 +0600

    Plan Management Route Added

---
+++ src/app/(adminLayout)/Admin/plan-management/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PlanManagementAdmin = () => {
+    return (
+        <div>
+            <h1>yge</h1>
+        </div>
+    );
+};
+
+export default PlanManagementAdmin;
