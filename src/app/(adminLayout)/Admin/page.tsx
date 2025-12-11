4415daed42c9ff953399fa21ab24524696bf095e
mdmasharafilhossain <mashrafilmahi007@gmail.com>
2025-12-11 22:59:46 +0600

    functionality updated

--- src/app/(adminLayout)/Admin/page.tsx
+++ src/app/(adminLayout)/Admin/page.tsx
@@ -2,9 +2,23 @@ import React from 'react';
 
 const AdminPage = () => {
     return (
-        <div>
-            <h1>Welcome to Admin Dashboard</h1>
+        <div className="min-h-screen bg-gray-100 flex items-center justify-center px-4">
+      <div className="bg-white max-w-lg w-full p-8 rounded-xl shadow-md border border-gray-200 text-center">
+        
+        <h1 className="text-3xl font-semibold text-gray-800">
+          Welcome to <span className='text-orange-600'>Admin Dashboard</span>
+        </h1>
+
+        <p className="text-gray-500 mt-3 text-sm">
+          We are glad to have you here. Manage all platform settings and all user activities.
+        </p>
+
+        <div className="mt-6">
+          <span className="text-4xl font-bold text-orange-500">👋</span>
         </div>
+
+      </div>
+    </div>
     );
 };
 
--- src/app/(userDashboard)/user/page.tsx
+++ src/app/(userDashboard)/user/page.tsx
@@ -1,11 +1,26 @@
-import React from 'react';
+
+
 
 const UserDashboardPage = () => {
-    return (
-        <div>
-            <h1>Welcome to User Dashboard</h1>
+  return (
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center px-4">
+      <div className="bg-white max-w-lg w-full p-8 rounded-xl shadow-md border border-gray-200 text-center">
+        
+        <h1 className="text-3xl font-semibold text-gray-800">
+          Welcome to <span className='text-orange-600'>User Dashboard</span>
+        </h1>
+
+        <p className="text-gray-500 mt-3 text-sm">
+          We are glad to have you here. Explore your travel activities and updates anytime.
+        </p>
+
+        <div className="mt-6">
+          <span className="text-4xl font-bold text-orange-500">👋</span>
         </div>
-    );
+
+      </div>
+    </div>
+  );
 };
 
 export default UserDashboardPage;
