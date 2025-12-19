f04420ddc43fbb60611430f70b25ea7a1adca8d2
mdmasharafilhossain <mashrafilmahi007@gmail.com>
2025-12-19 19:15:32 +0600

    bug fixed

--- src/app/(commonLayout)/explore/page.tsx
+++ src/app/(commonLayout)/explore/page.tsx
@@ -278,12 +278,7 @@ function MatchCard({ plan }: { plan: MatchPlanCommon }) {
           >
             View Plan
           </Link>
-          <Link
-            href={`/profile/${plan.host.id}`}
-            className="text-[10px] text-gray-500 hover:text-gray-700"
-          >
-            View Profile
-          </Link>
+          
         </div>
       </div>
     </div>
