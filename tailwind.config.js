4d98e874860c94d908130b1675a78a0b047f6deb
mdmasharafilhossain <mashrafilmahi007@gmail.com>
2026-01-21 13:28:54 +0600

    new tailwind CSS file added

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export const darkMode = "class";
+export const content = [
+    "./src/app/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}",
+];
+export const theme = {
+    extend: {},
+};
+export const plugins = [];
